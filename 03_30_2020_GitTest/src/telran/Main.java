0550eac1cae6d70deac9374d98c0ca9f495ba928
DiBDV <bdv.dmitry@gmail.com>
2020-03-30 20:56:21 +0200

    adding new method

--- 03_30_2020_GitTest/src/telran/Main.java
+++ 03_30_2020_GitTest/src/telran/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello to the world!");
     }
+
+    //    test task for pull request
+    public static void NewMethod(){
+        System.out.println("This is a new method");
+    }
 }
