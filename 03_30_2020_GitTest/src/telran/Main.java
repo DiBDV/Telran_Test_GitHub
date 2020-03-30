3baf39720dd8a615b40a3b8bf28e65b0262cfcda
DiBDV <bdv.dmitry@gmail.com>
2020-03-30 21:15:20 +0200

    I can create description for the issue

--- 03_30_2020_GitTest/src/telran/Main.java
+++ 03_30_2020_GitTest/src/telran/Main.java
@@ -3,10 +3,8 @@ package telran;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello to the world!");
+//        another test pull request
+        System.out.println("Yeahoo, I can create another pull request with description");
     }
 
-    //    test task for pull request
-    public static void newMain(String[] args) {
-        System.out.println("This is a new method");
-    }
 }
