a8b1495f542148afdbcea8633d960ff444c4a8e8
DiBDV <bdv.dmitry@gmail.com>
2020-03-30 21:22:02 +0200

    adding new commit

--- 03_30_2020_GitTest/src/telran/Main.java
+++ 03_30_2020_GitTest/src/telran/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello to the world!");
 //        another test pull request
         System.out.println("Yeahoo, I can create another pull request with description");
+        System.out.println();
     }
 
 }
